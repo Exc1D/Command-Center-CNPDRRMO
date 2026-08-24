fa99a8198e5e9b237cf2933d35bba5043c9fdfd8
Exc1D <davidaviado.dla@gmail.com>
2026-08-24 10:05:00 +0800

    Replace broken Geoman masked icons

--- scripts/check-geoman-icons.mjs
+++ scripts/check-geoman-icons.mjs
@@ -7,10 +7,7 @@ const css = fs.readdirSync(assets)
   .filter(file => file.endsWith('.css'))
   .map(file => fs.readFileSync(path.join(assets, file), 'utf8'))
   .join('\n');
-const missing = icons.filter(icon => !css.includes(`.leaflet-pm-icon-${icon}`));
-const brokenMasks = css.match(/mask="url\('%23/g) ?? [];
+const missing = icons.filter(icon => !new RegExp(`\\.leaflet-pm-icon-${icon}\\{background-image:url\\([^)]*%3Csvg`).test(css));
 
-if (missing.length || brokenMasks.length) {
-  throw new Error(`Broken Geoman icons: ${missing.join(', ') || `${brokenMasks.length} invalid SVG masks`}`);
-}
+if (missing.length) throw new Error(`Broken Geoman icons: ${missing.join(', ')}`);
 console.log(`Verified ${icons.length} Geoman icons`);
